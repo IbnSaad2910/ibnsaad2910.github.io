03910a6cb4798f53364a2ed63c9a997a65108360
IbnSaad2910 <alif2910@gmail.com>
2025-11-06 09:04:41 +0800

    chore: update site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 import tailwindcss from "@tailwindcss/vite";
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+  site: "Data Communication and Computer Networks",
   integrations: [mdx(), sitemap()],
 
   vite: {
