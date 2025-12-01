10cbfdef54903e54fcf17622db041314d7e3a5d0
Muhammad Alif Akmal <muhammadalif@atquest.com>
2025-12-01 17:14:11 +0800

    feat: update work experience

--- src/data/work.ts
+++ src/data/work.ts
@@ -11,9 +11,17 @@ export const workIcons = {
 
 export const work = [
   {
+    title: "Sofware Developer",
+    company: "atQuest Solutions Sdn Bhd",
+    region: "Petaling Jaya, Selangor",
+    description:
+      "Learn and contribute to .NET backend (ASP.NET Core Web API) under guidance: routes, controllers, services, DI, logging. Fix bugs, write small enhancements, and help with QA, code reviews, and documentation. Support real enterprise features: authentication/authorization, validation, error handling, and basic performance checks.",
+    technologies: [".NET", "MS SQL"],
+  },
+  {
     title: "Full Stack Developer",
     company: "Nexlaw Pty Ltd",
-    region: "Malaysia",
+    region: "Puchong, Selangor",
     description:
       "Developed a Microsoft Word Add-In integrated with the NexLaw platform to help lawyers streamline contract review and drafting processes. Designed and managed the web application’s back-end, including database architecture and API integration within AWS infrastructure. Built and deployed the front-end interface to AWS, ensuring a responsive and user-friendly experience. Continuously debugged, maintained, and enhanced the application’s overall robustness, performance, and reliability to support seamless user operations.",
     technologies: ["Laravel", "MySQL", "Vue", "AWS", "Python", "Axios", "Nuxt"],
@@ -21,7 +29,7 @@ export const work = [
   {
     title: "Full Stack Developer",
     company: "Nexlaw Pty Ltd",
-    region: "Malaysia",
+    region: "Puchong, Selangor",
     description:
       "Developed the back-end APIs and front-end interface for the NexIntelligence Audience Intelligence feature within the NexMind platform. Designed and managed the web application’s back-end, including database architecture and API integration with AWS infrastructure. Built and deployed the front-end components to AWS, ensuring a responsive and intuitive user experience. Continuously debugged, optimized, and enhanced the application’s performance, stability, and reliability to support data-driven analytics and seamless functionality.",
     technologies: ["Laravel", "MySQL", "Vue", "AWS", "Python"],
@@ -29,7 +37,7 @@ export const work = [
   {
     title: "Web Developer",
     company: "NexMind AI Sdn Bhd",
-    region: "Malaysia",
+    region: "Puchong, Selangor",
     description:
       "Maintained and managed web hosting environments and DNS configurations for both the company and its clients. Oversaw and enhanced websites built on platforms such as WordPress, Shopify, OpenCart, and Wix, ensuring optimal performance and user experience. Managed user accounts and handled application configuration and troubleshooting through Office 365, supporting smooth day-to-day operations and client service delivery.",
     technologies: [
